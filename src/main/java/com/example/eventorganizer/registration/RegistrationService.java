019cdc88cbee28faadc20117a2e5d0808f0280a2
Dachi <dachi.laperashvili@gmail.com>
2023-06-25 16:46:16 +0400

    feat(!): added registration page

---
+++ src/main/java/com/example/eventorganizer/registration/RegistrationService.java
@@ -0,0 +1,28 @@
+package com.example.eventorganizer.registration;
+
+import com.example.eventorganizer.user.User;
+import com.example.eventorganizer.user.UserRepository;
+import com.example.eventorganizer.user.UserRole;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public String register(RegistrationRequest request){
+        User user = new User(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                bCryptPasswordEncoder.encode(request.getPassword()),
+                UserRole.USER
+        );
+
+        userRepository.save(user);
+        return "Registration successful";
+    }
+}
