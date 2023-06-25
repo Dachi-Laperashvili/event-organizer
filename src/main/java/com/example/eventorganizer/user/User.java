77bd5e971533d0f8568e52b3d42562dc142afc80
Dachi <dachi.laperashvili@gmail.com>
2023-06-25 16:10:03 +0400

    feat: updated users entity

--- src/main/java/com/example/eventorganizer/user/User.java
+++ src/main/java/com/example/eventorganizer/user/User.java
@@ -2,25 +2,37 @@ package com.example.eventorganizer.user;
 
 import com.example.eventorganizer.event.Event;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
     @ManyToMany(mappedBy = "users")
     private Set<Event> events = new HashSet<>();
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
-    public User(String email, String password) {
+    public User(String firstName, String lastName, String email, String password, UserRole role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
-
-    public User() {}
 }
