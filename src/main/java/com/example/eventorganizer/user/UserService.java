4df9460895c00a84803b95020e1d5529979b95ea
Dachi <dachi.laperashvili@gmail.com>
2023-06-27 15:23:09 +0400

    refactor: removed transactional annotation from userRepository

--- src/main/java/com/example/eventorganizer/user/UserService.java
+++ src/main/java/com/example/eventorganizer/user/UserService.java
@@ -5,8 +5,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @AllArgsConstructor
 public class UserService  implements UserDetailsService {
     private final static String USER_NOT_FOUND = "User with email %s not found";
