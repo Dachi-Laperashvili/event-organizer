6ff29c6756aedd2ea80ce1f99b8fb7cce1a582d7
Dachi <dachi.laperashvili@gmail.com>
2023-07-11 10:25:54 +0400

    refactor: used java 8+ collection methods instead of creating new hashmap

--- src/main/java/com/example/eventorganizer/task/ContributionService.java
+++ src/main/java/com/example/eventorganizer/task/ContributionService.java
@@ -7,25 +7,19 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ContributionService {
     public Map<User, BigDecimal> calculateContributions(Event event, User spender, BigDecimal spentMoney){
-        List<User> participants = new ArrayList<>(event.getUsers());
-        participants.remove(spender);
+        List<User> participants = event.getUsers().stream().filter(user -> !user.equals(spender)).toList();
 
         int numParticipants = participants.size() + 1;
         if( numParticipants == 1){
-            return Collections.emptyMap();
+            return Map.of();
         }
 
         BigDecimal contributionPerPerson = spentMoney.divide(BigDecimal.valueOf(numParticipants),2, RoundingMode.HALF_UP);
-
-        Map<User, BigDecimal> contributions = new HashMap<>();
-
-        for(User participant: participants){
-            contributions.put(participant,contributionPerPerson);
-        }
-        return contributions;
+        return participants.stream().collect(Collectors.toMap(user -> user,user -> contributionPerPerson));
     }
 }
